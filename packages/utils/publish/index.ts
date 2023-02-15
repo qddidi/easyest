eb2b418f2ae8a826b0d6d1a06a313838489135dd
张迪生 <2046353594@qq.com>
2023-02-15 15:08:34 +0800

    feat: add release

--- packages/utils/publish/index.ts
+++
@@ -1,7 +0,0 @@
-import run from "../../components/script/utils/run";
-import { pkgPath } from "../../components/script/utils/paths";
-import { series } from "gulp";
-export const publishComponent = async () => {
-  run("release-it", `.`);
-};
-export default series(async () => publishComponent());
