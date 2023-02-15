7b408976fa55248253d7642208aed510e67e2e00
张迪生 <2046353594@qq.com>
2023-02-15 16:17:11 +0800

    fix: 忽略@easyest/utils

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     //minify: false,
     rollupOptions: {
       //忽略打包vue文件
-      external: ["vue", /\.less/],
+      external: ["vue", /\.less/, "@easyest/utils"],
       input: ["index.ts"],
       output: [
         {
