1ad5c033242612380750233201f6921c776418cc
张迪生 <2046353594@qq.com>
2023-02-20 14:24:03 +0800

    feat: cli

---
+++ packages/cli/cli.js
@@ -0,0 +1,89 @@
+import commandLineArgs from "command-line-args"
+import commandLineUsage from "command-line-usage"
+import gitClone from "./utils/gitClone.js"
+import prompts from "prompts"
+import { readFile } from 'fs/promises'
+
+const pkg = JSON.parse(
+  await readFile(
+    new URL('./package.json', import.meta.url)
+  )
+)
+//配置命令参数
+const optionDefinitions = [
+  { name: 'version', alias: 'v', type: Boolean },
+  { name: 'help', alias: 'h', type: Boolean }
+];
+
+//帮助命令
+const helpSections = [
+  {
+    header: 'create-easyest',
+    content: '一个快速生成组件库搭建环境的脚手架',
+  },
+  {
+    header: 'Options',
+    optionList: [
+      {
+        name: 'version',
+        alias: 'v',
+        typeLabel: '{underline boolean}',
+        description: '版本号',
+      },
+      {
+        name: 'help',
+        alias: 'h',
+        typeLabel: '{underline boolean}',
+        description: '帮助',
+      }
+    ],
+  },
+];
+const promptsOptions = [
+  {
+    type: 'text',
+    name: 'name',
+    message: '请输入项目名称'
+  },
+  {
+    type: 'select',//单选
+    name: 'template',
+    message: '请选择一个模板',
+    choices: [
+      { title: 'kitty-ui', value: 0 },
+      { title: 'easyest', value: 1 }
+    ]
+  }
+]
+const options = commandLineArgs(optionDefinitions);
+
+
+
+const remoteList = {
+  1: "https://gitee.com/geeksdidi/kittyui.git",
+  2: 'https://github.com/qddidi/easyest.git'
+}
+const branch = 'master'
+
+const getUserInfo = async () => {
+  const res = await prompts(promptsOptions)
+  if (!res.name || !res.template) return
+  gitClone(`direct:${remoteList[res.template]}#${branch}`, res.name, { clone: true })
+}
+const runOptions = () => {
+  if (options.version) {
+    console.log(`v${pkg.version}`)
+    return
+  }
+  if (options.help) {
+    console.log(commandLineUsage(helpSections))
+    return
+  }
+  getUserInfo()
+}
+
+runOptions()
+
+
+
+
--- packages/components/src/button/index.ts
+++ packages/components/src/button/index.ts
@@ -1,5 +1,4 @@
 import _Button from "./button.vue";
-import type { App, Plugin } from "vue";
 import { withInstall } from "@easyest/utils";
 export const Button = withInstall(_Button);
 export default Button;
