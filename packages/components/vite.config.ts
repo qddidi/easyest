5ef9cda6fcd5780d4f39f47141ef084ea5683603
张迪生 <2046353594@qq.com>
2023-03-13 16:37:54 +0800

    feat: 新增Vitest单元测试

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,9 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 // @ts-ignore
 import DefineOptions from 'unplugin-vue-define-options/vite';
 export default defineConfig({
+  test: {
+    environment: 'happy-dom'
+  },
   build: {
     //压缩
     //minify: false,
