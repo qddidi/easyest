4d4debc98b348918b5b0758cba6f8065e096d2ec
张迪生 <2046353594@qq.com>
2023-04-03 14:29:49 +0800

    feat: support tsx

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+// @ts-ignore
 import DefineOptions from 'unplugin-vue-define-options/vite';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 export default defineConfig({
-  plugins: [vue(), DefineOptions()]
+  plugins: [vue(), DefineOptions(), vueJsx()]
 });
